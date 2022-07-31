0133f56ccdf290c4b9a12dd7fcf0f5937bcb2c24
surya-karthik <78813375+surya-karthik@users.noreply.github.com>
2022-07-31 22:04:39 +0530

    Update Vowels5.py

--- Vowels5.py
+++ Vowels5.py
@@ -1,3 +1,7 @@
+#Program to print vowels using intersection method in sets..
+#intersection method in sets ,takes the objects in one set and compares them to those in another, then reports on any common objects found. 
+
+
 vowels = {'a','e','i','o','u'}
 #vowels = set('aeiou')
 word = input("Provide a word to search for vowels: ")
