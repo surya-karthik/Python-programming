00be798cfa0ea66f9ff13624ef084da5e72b3307
surya-karthik <78813375+surya-karthik@users.noreply.github.com>
2022-07-31 19:40:57 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,11 @@
+#Using Intersection method in Sets {}
+
 vowels = {'a','e','i','o','u'}
-word = input("type your word, ")
+word = input("type your word: ")
+
+
+#intersection method in sets ,takes the objects in one set and compares them to those in another, then reports on any common objects found. 
+
 
 r = vowels.intersection(set(word))
 print(r)
