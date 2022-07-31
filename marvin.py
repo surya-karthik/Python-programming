2d448bd4344b5890a4d7b8deb84787cd1abdc52e
surya-karthik <78813375+surya-karthik@users.noreply.github.com>
2022-07-31 19:22:09 +0530

    Update marvin.py

--- marvin.py
+++ marvin.py
@@ -4,3 +4,8 @@ letters = list(paranoid_android)
 
 for char in letters:
     print('\t', char)
+    
+    
+    
+#Each character from the “letters” list is printed on its own line, preceded by a tab character (that’s what the \t does).
+
