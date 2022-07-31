689fe0bf72180b89ddc4bd30c0ce2c3b9dd4eba3
surya-karthik <78813375+surya-karthik@users.noreply.github.com>
2022-07-31 19:30:55 +0530

    Update marvin2.py

--- marvin2.py
+++ marvin2.py
@@ -2,13 +2,22 @@ paranoid_android = "Marvin, the Paranoid Android"
 
 letters = list(paranoid_android)
 
+#The first loop iterates over a slice of the first six objects in the list.
+
 for char in letters[:6]:
     print('\t', char)
 print()
+
+
+#The second loop iterates over a slice of the last seven objects in the list. Here “*2” inserts two tab characters before each printed object.
 
 for char in letters[-7:]:
     print('\t'*2,char)
 print()
 
-for char in letters[12:20]:
+
+#The third loop iterates over a slice  from within the list, selecting the characters that spell the word “Paranoid” in the list.
+#Here “*3” inserts three tab characters before each printed object.
+
+ for char in letters[12:20]:
     print('\t'*3, char)
