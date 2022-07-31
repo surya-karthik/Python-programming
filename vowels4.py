2d36fa3a49dae2b0988317851b5cac2bf64918e0
surya-karthik <78813375+surya-karthik@users.noreply.github.com>
2022-07-31 22:00:27 +0530

    Update vowels4.py

--- vowels4.py
+++ vowels4.py
@@ -10,6 +10,18 @@ for letter in word:
     if letter in vowels:
         found.setdefault(letter,0)
         found[letter] +=1
+
+
+
+        
         
 for k, V in sorted(found.items()):
     print(k, "was found" ,V, "time(s).")
+
+    
+    
+   
+# We used setdefault method to initialize a nonexistent key to a supplied default value.“setdefault” is also used to  avoid the “KeyError” exception.
+#The setdefault method does what the two lines if/ not in statements do, by using a single line of code.
+
+                                     
