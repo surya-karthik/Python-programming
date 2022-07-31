3d79cccee1df5c1df8427b75c7586d41bb5bd346
surya-karthik <78813375+surya-karthik@users.noreply.github.com>
2022-07-31 19:20:45 +0530

    Update panic2.py

--- panic2.py
+++ panic2.py
@@ -1,3 +1,5 @@
+#Program to transform the string "Don't panic!" into "on tap" using slicing a List concept!"
+
 phrase = "Don't panic!"
 plist = list(phrase)
 print(phrase) 
@@ -5,9 +7,6 @@ print(plist)
 
 new_phrase = ''.join(plist[1:3])
 new_phrase = new_phrase + ''.join([plist[5],plist[4],plist[7],plist[6]])
-
-
-
 
 
 print(plist)
